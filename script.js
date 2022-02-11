7ce90eb6da2fd4bfb68eb15da4879787757b3690
João Victor Peres Silva <vitorr-joao2222@hotmail.com>
2022-02-10 22:03:44 -0300

    calculator-upgrade-10-02-22

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 const inputBill = document.querySelector('input[name="bill"]');
 const inputPeople = document.querySelector('input[name="people"]');
 
-inputBill.addEventListener("keyup", () => {
+function calc() {
   const value = document.querySelector('input[name="bill"]').value;
   const key = document.querySelector('input[name="people"]').value;
   const amount = document.querySelector(".amount p");
   const divider = Number(value) / Number(key);
 
-  amount.innerHTML = `$${Number(divider).toFixed(2)}`;
-});
+  if (key) {
+    amount.innerHTML = `$${Number(divider).toFixed(2)}`;
+  }
+}
 
-inputPeople.addEventListener("keyup", () => {
-  const value = document.querySelector('input[name="bill"]').value;
-  const key = document.querySelector('input[name="people"]').value;
-  const amount = document.querySelector(".amount p");
-  const divider = Number(value) / Number(key);
-
-  amount.innerHTML = `$${Number(divider).toFixed(2)}`;
-});
+inputBill.addEventListener("keyup", calc);
+inputPeople.addEventListener("keyup", calc);
 
 // const result = calc(value, key);
 
